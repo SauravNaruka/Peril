3d3c44c21d0f068ea7543ed8a955197bf1af6e6c
SauravNaruka <saurav.singh.n@gmail.com>
2025-01-03 12:00:18 +0530

    Add basic server and client code

--- internal/gamelogic/pause.go
+++ internal/gamelogic/pause.go
@@ -3,7 +3,7 @@ package gamelogic
 import (
 	"fmt"
 
-	"github.com/bootdotdev/learn-pub-sub-starter/internal/routing"
+	"github.com/SauravNaruka/Peril/internal/routing"
 )
 
 func (gs *GameState) HandlePause(ps routing.PlayingState) {
