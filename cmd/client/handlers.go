23ece121d51c25155f789d38d6b801a9269c8c48
SauravNaruka <saurav.singh.n@gmail.com>
2025-01-04 13:00:03 +0530

    Add client move publish events

--- cmd/client/handlers.go
+++ cmd/client/handlers.go
@@ -10,7 +10,13 @@ import (
 func handlerPause(gs *gamelogic.GameState) func(routing.PlayingState) {
 	return func(ps routing.PlayingState) {
 		defer fmt.Print("> ")
-
 		gs.HandlePause(ps)
+	}
+}
+
+func handlerMove(gs *gamelogic.GameState) func(move gamelogic.ArmyMove) {
+	return func(move gamelogic.ArmyMove) {
+		defer fmt.Print("> ")
+		gs.HandleMove(move)
 	}
 }
--- cmd/client/main.go
+++ cmd/client/main.go
@@ -13,9 +13,10 @@ import (
 )
 
 type config struct {
-	connection *amqp.Connection
-	username   string
-	gameState  gamelogic.GameState
+	connection     *amqp.Connection
+	publishChannel *amqp.Channel
+	username       string
+	gameState      gamelogic.GameState
 }
 
 func main() {
@@ -36,12 +37,29 @@ func main() {
 	defer conn.Close()
 	fmt.Println("Peril game client connected to RabbitMQ!")
 
+	publishCh, err := conn.Channel()
+	if err != nil {
+		log.Fatalf("could not create channel: %v", err)
+	}
+
 	userName, err := gamelogic.ClientWelcome()
 	if err != nil {
 		log.Fatalf("Peril game client couldn't get username: %v", err)
 	}
 
 	gs := gamelogic.NewGameState(userName)
+
+	err = pubsub.SubscribeJSON(
+		conn,
+		routing.ExchangePerilTopic,
+		routing.ArmyMovesPrefix+"."+gs.GetUsername(),
+		routing.ArmyMovesPrefix+".*",
+		pubsub.SimpleQueueTransient,
+		handlerMove(gs),
+	)
+	if err != nil {
+		log.Fatalf("could not subscribe to army moves: %v", err)
+	}
 
 	err = pubsub.SubscribeJSON(
 		conn,
@@ -56,17 +74,13 @@ func main() {
 	}
 
 	cfg := &config{
-		connection: conn,
-		username:   userName,
-		gameState:  *gs,
+		connection:     conn,
+		publishChannel: publishCh,
+		username:       userName,
+		gameState:      *gs,
 	}
 
 	cfg.repl()
-
-	// // wait for ctrl+c
-	// signalChan := make(chan os.Signal, 1)
-	// signal.Notify(signalChan, os.Interrupt)
-	// <-signalChan
 }
 
 func (cfg *config) repl() {
@@ -84,11 +98,23 @@ func (cfg *config) repl() {
 				continue
 			}
 		case "move":
-			_, err := cfg.gameState.CommandMove(words)
+			move, err := cfg.gameState.CommandMove(words)
 			if err != nil {
 				fmt.Println(err)
 				continue
 			}
+			routingKey := routing.ArmyMovesPrefix + "." + cfg.gameState.GetUsername()
+			err = pubsub.PublishJSON(
+				cfg.publishChannel,
+				routing.ExchangePerilTopic,
+				routingKey,
+				move,
+			)
+			if err != nil {
+				fmt.Printf("error while publishing changes to exchange=%s key=%s. Error: %v\n", routing.ExchangePerilTopic, routingKey, err)
+				continue
+			}
+			fmt.Printf("Moved %v units to %s\n", len(move.Units), move.ToLocation)
 		case "status":
 			cfg.gameState.CommandStatus()
 		case "help":
